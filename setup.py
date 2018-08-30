6491d982fcbf37eadc8b3fce2bc97925a565d62d
joaopteixeira <joaopvilares@hotmail.com>
2018-08-30 10:21:09 +0100

    v1.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SpaceShip Brench Backend',
+    version='',
+    packages=['CoreGame'],
+    url='',
+    license='',
+    author='SaraS',
+    author_email='',
+    description=''
+)
